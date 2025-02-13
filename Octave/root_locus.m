a3c2e240c4228f1ee82cce8a359947dafa286fe2
Yadukrishnan KM <140938944+YadukrishnanKM@users.noreply.github.com>
2025-02-13 22:02:25 +0530

    Update root_locus.m

--- Octave/root_locus.m
+++ Octave/root_locus.m
@@ -6,9 +6,9 @@ clc
 numerator   =   [1]
 denominator =   [1 7 24 9]
 
-# Create the transfer function
+% Create the transfer function
 system = tf(numerator, denominator)
 
-# Plot the root locus
+% Plot the root locus
 rlocus(system)
 line ([0,0],[-10, 10], "linewidth", 2, "color", 'k')
